44c3c16213db5754df08c1b7c044b48a285fff29
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2023-10-30 18:08:47 +0530

    add some function

--- src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
+++ src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { AiOutlineFullscreen, AiOutlineFullscreenExit, AiOutlineSetting } from "react-icons/ai";
 import { ISettings } from "../Playground";
 import SettingsModal from "@/components/Modals/SettingsModal";
+import { BsChevronDown } from "react-icons/bs";
 
 type PreferenceNavProps = {
 	settings: ISettings;
@@ -40,9 +41,10 @@ const PreferenceNav: React.FC<PreferenceNavProps> = ({ setSettings, settings }) 
 	return (
 		<div className='flex items-center justify-between bg-dark-layer-2 h-11 w-full '>
 			<div className='flex items-center text-white'>
-				<button className='flex cursor-pointer items-center rounded focus:outline-none bg-dark-fill-3 text-dark-label-2 hover:bg-dark-fill-2  px-2 py-1.5 font-medium'>
+				<button className='flex cursor-pointer items-center rounded focus:outline-none bg-gray-600 text-dark-label-2 hover:bg-gray-700 px-2 py-1.5 font-medium'>
 					<div className='flex items-center px-1'>
-						<div className='text-xs text-label-2 dark:text-dark-label-2'>JavaScript</div>
+						<div className='text-xs text-label-2 mr-2 dark:text-dark-label-2'>JavaScript</div>
+						<BsChevronDown className='fill-white' />
 					</div>
 				</button>
 			</div>
